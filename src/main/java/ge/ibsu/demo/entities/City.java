2c4f46dbb5014d471b316a0918f0efc38bc4ee91
barrbare <22100512@ibsu.edu.ge>
2024-01-17 12:10:14 +0400

    b

--- src/main/java/ge/ibsu/demo/entities/City.java
+++ src/main/java/ge/ibsu/demo/entities/City.java
@@ -2,6 +2,8 @@ package ge.ibsu.demo.entities;
 
 import jakarta.persistence.*;
 
+@Entity
+@Table(name = "city")
 public class City {
     @Id
     @SequenceGenerator(name = "city_city_id_seq", sequenceName = "city_city_id_seq", allocationSize = 1)
