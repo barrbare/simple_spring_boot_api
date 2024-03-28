d1275a216061b5f5b68b3ec210bfe65933c7b00a
barrbare <22100512@ibsu.edu.ge>
2024-03-29 01:24:28 +0400

    ვაიმე

--- src/main/java/ge/ibsu/demo/services/ActorService.java
+++ src/main/java/ge/ibsu/demo/services/ActorService.java
@@ -15,6 +15,6 @@ public class ActorService {
     }
 
     public List<SearchActor> getAllActorNames() {
-        return actorRepository.findAllActorsNames();
+        return  actorRepository.findAllActorsNames();
     }
 }
